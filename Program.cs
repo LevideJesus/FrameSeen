59eae4a32e2c229e7e436158fe140308b7e85730
Levi de Jesus da Silva Francelino <levizao@Levis-MacBook-Air.local>
2026-08-17 10:16:01 -0300

    remove: removed weather files

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 
+using Scalar.AspNetCore;
+
 namespace FrameSeen
 {
     public class Program
@@ -19,6 +21,7 @@ namespace FrameSeen
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.MapScalarApiReference();
             }
 
             app.UseHttpsRedirection();
