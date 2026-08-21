c849cedc0798ed7d70ffdacc8b70bebbbfc5d321
Levi de Jesus da Silva Francelino <levizao@Levis-MacBook-Air.local>
2026-08-20 21:16:15 -0300

    feature: Add EF Core and SQL Server with initial Series migration

---
+++ Migrations/20260820232851_initial.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FrameSeen.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Series",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Overview = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PosterPath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumberOfSeasons = table.Column<int>(type: "int", nullable: false),
+                    NumberOfEpisodes = table.Column<int>(type: "int", nullable: false),
+                    EpisodeRunTime = table.Column<int>(type: "int", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstAirDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Series", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Series");
+        }
+    }
+}
