7953a4d826390bd836d72c9790dab15150e58530
Levi de Jesus da Silva Francelino <levizao@Levis-MacBook-Air.local>
2026-08-29 21:30:03 -0300

    feature: add httpPost and HttpPut by Id in User Controller

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,3 +1,5 @@
+using FrameSeen.Dtos;
+using FrameSeen.Models;
 using FrameSeen.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,6 +12,53 @@ namespace FrameSeen.Controllers
         public UsersController(IUserService userService)
         {
             service = userService;
+        }
+        [HttpGet()]
+        public IActionResult GetUsers()
+        {
+
+            return Ok(service.GetAllUsers());
+        }
+
+        [HttpGet ("{id}")]
+  
+        public IActionResult GetUsersById(int id)
+        {
+            var response = service.GetUsersById(id);
+
+            if(response == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost]
+
+        public IActionResult CreateSerie(UserRequest user)
+        {
+            var createdUser = service.AddUsers(user);
+            return CreatedAtAction(nameof(GetUsersById), new {id = createdUser.Id}, createdUser);
+        }
+
+        [HttpPut("{id}")]
+        
+
+        public IActionResult UpdateSerie(int id, User serie)
+        {
+            try
+            {
+                service.UpdateUsers(id, serie);
+
+                return NoContent();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+            
+
         }
     }
 }
