f6f5dcd127c46e6499ee3fdbd3d377eeafb09985
Levi de Jesus da Silva Francelino <levizao@Levis-MacBook-Air.local>
2026-09-01 19:17:23 -0300

    feature: migtration implemented to the SQL

---
+++ Migrations/20260901221132_InitialDiaries.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FrameSeen.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDiaries : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Diaries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    SeriesId = table.Column<int>(type: "int", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: true),
+                    WatchedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    Review = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diaries", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Diaries");
+        }
+    }
+}
