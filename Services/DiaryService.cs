8e4f8f7a51169f9ed85c66b1f92db661ece63ef8
Levi de Jesus da Silva Francelino <levizao@Levis-MacBook-Air.local>
2026-09-01 20:10:34 -0300

    feature: GetAllDiaries implemented in the DiaryService

--- Services/DiaryService.cs
+++ Services/DiaryService.cs
@@ -1,9 +1,34 @@
+using FrameSeen.Data;
+using FrameSeen.Dtos;
 using FrameSeen.Models;
 
 namespace FrameSeen.Services
 {
     public class DiaryService : IDiaryService
     {
+        private readonly AppDbContext context;
         
+        public DiaryService(AppDbContext appDbContext)
+        {
+            context = appDbContext;
+        }
+
+        public IEnumerable<DiaryResponse> GetAllDiaries()
+        {
+            var diaries = context.Diaries.ToList();
+
+            var response = diaries.Select(d => new DiaryResponse
+            {
+                Id = d.Id,
+                UserId = d.UserId,
+                SeriesId = d.SeriesId,
+                Rating = d.Rating,
+                WatchedAt = d.WatchedAt,
+                Review = d.Review,
+                CreatedAt = d.CreatedAt
+            });
+
+            return response;
+        }
     }
 }
