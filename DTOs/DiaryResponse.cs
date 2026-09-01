97d478656164a11fac0e5d151554bcb5236077fd
Levi de Jesus da Silva Francelino <levizao@Levis-MacBook-Air.local>
2026-09-01 19:39:34 -0300

    add: fields to Diary Response too

--- DTOs/DiaryResponse.cs
+++ DTOs/DiaryResponse.cs
@@ -2,6 +2,18 @@ namespace FrameSeen.Dtos
 {
     public class DiaryResponse
     {
-        
+        public int Id {get; set;}
+
+        public int UserId {get; set;}
+
+        public int SeriesId {get; set;}
+
+        public int? Rating {get; set;}
+
+        public DateTimeOffset WatchedAt {get; set;}
+
+        public string? Review {get; set;}
+
+        public DateTimeOffset CreatedAt {get; set;}
     }
 }
