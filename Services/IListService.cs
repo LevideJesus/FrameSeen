ff63ba70e77b8cc409014fb0d89e17ccba14e36b
Levi de Jesus da Silva Francelino <levizao@Levis-MacBook-Air.local>
2026-09-04 15:44:02 -0300

    add: IListService properties configured

--- Services/IListService.cs
+++ Services/IListService.cs
@@ -1,7 +1,15 @@
+using FrameSeen.Dtos;
+using FrameSeen.Models;
 namespace FrameSeen.Services
 {
     public interface IListService
     {
-        
+        IEnumerable<ListResponse> GetAllLists(int userId);
+
+        ListResponse? GetListById(int id);
+
+        ListResponse AddList(ListRequest request);
+        ListResponse? UpdateList(int id, ListRequest request);
+        void DeleteList(int id);
     }
 }
