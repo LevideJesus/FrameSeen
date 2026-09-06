21ff756d1a48f5e024f4a9559c5a0c1c2a8f3f37
Levi de Jesus da Silva Francelino <levizao@Levis-MacBook-Air.local>
2026-09-06 10:38:14 -0300

    feature: get all lists controller done

--- Controllers/ListController.cs
+++ Controllers/ListController.cs
@@ -1,7 +1,45 @@
+using System.Security.Claims;
+using FrameSeen.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+
+
 namespace FrameSeen.Controllers
 {
-    public class ListController
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListController : ControllerBase
     {
-        
+        private readonly IListService service;
+
+        public ListController(IListService listService)
+        {
+            service = listService;
+        }
+
+        [Authorize]
+        [HttpGet]
+
+        public IActionResult GetAllLists()
+        {
+            var nameIdentifier = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(nameIdentifier))
+            {
+                return Unauthorized();
+            }
+
+            if(!int.TryParse(nameIdentifier, out int userId))
+            {
+                return BadRequest();
+            }
+
+            var lists = service.GetAllLists(userId);
+
+            return Ok(lists);
+        }
     }
 }
