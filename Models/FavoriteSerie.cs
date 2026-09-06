31cd3772da4fc71263d1f2c99a12d7d805eb84c0
Levi de Jesus da Silva Francelino <levizao@Levis-MacBook-Air.local>
2026-09-06 11:45:23 -0300

    add: favorite series model fields added

--- Models/FavoriteSerie.cs
+++ Models/FavoriteSerie.cs
@@ -2,6 +2,10 @@ namespace FrameSeen.Models
 {
     public class FavoriteSerie
     {
-        
+        public int Id {get; set;}
+
+        public int ListId {get; set;}
+
+        public int SeriesId {get; set;}
     }
 }
